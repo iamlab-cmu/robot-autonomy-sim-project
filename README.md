5cd4a5904f8d17db96631bad45c14b37caea2090
Jacky Liang <jackyliang@cmu.edu>
2020-03-25 14:51:12 -0400

    added example script added readme added requirements

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# robot-autonomy-sim-project
+# Robot Autonomy Spring 2020 Simulation Project
+
+## Installation
+
+Please use Python 3.6
+
+1. Install [PyRep](https://github.com/stepjam/PyRep)
+2. Install [RLBench](https://github.com/stepjam/RLBench)
+3. `pip install -r requirements.txt`
+
+## Example RLBench Usage
+Run `python rlbench_example.py` to launch the example script.
+Here, the `BlockPyramid` task is used, and the policy is random end-effector positions.
+
+This script contains example code on how to control the robot, get observations, and get noisy object pose readings.
+
+## Useful Files
+The following files may be useful to reference from the In the `rlbench` folder in the `RLBench` repo:
+* `rlbench/action_modes.py` - Different action modes to control the robot
+* `rlbench/backend/observation.py` - All fields available in the observation object
---
+++ rlbench_example.py
@@ -0,0 +1,93 @@
+import numpy as np
+import scipy as sp
+from quaternion import from_rotation_matrix, quaternion
+
+from rlbench.environment import Environment
+from rlbench.action_modes import ArmActionMode, ActionMode
+from rlbench.observation_config import ObservationConfig
+from rlbench.tasks import StackCups, StackBlocks, BlockPyramid, SweepToDustpan
+
+def skew(x):
+    return np.array([[0, -x[2], x[1]],
+                    [x[2], 0, -x[0]],
+                    [-x[1], x[0], 0]])
+
+
+def sample_normal_pose(pos_scale, rot_scale):
+    '''
+    Samples a 6D pose from a zero-mean isotropic normal distribution
+    '''
+    pos = np.random.normal(scale=pos_scale)
+        
+    eps = skew(np.random.normal(scale=rot_scale))
+    R = sp.linalg.expm(eps)
+    quat_wxyz = from_rotation_matrix(R)
+
+    return pos, quat_wxyz
+
+
+class RandomAgent:
+
+    def act(self, obs):
+        delta_pos = [(np.random.rand() * 2 - 1) * 0.005, 0, 0]
+        delta_quat = [0, 0, 0, 1] # xyzw
+        gripper_pos = [np.random.rand() > 0.5]
+        return delta_pos + delta_quat + gripper_pos
+
+
+class NoisyObjectPoseSensor:
+
+    def __init__(self, env):
+        self._env = env
+
+        self._pos_scale = [0.005] * 3
+        self._rot_scale = [0.01] * 3
+
+    def get_poses(self):
+        objs = self._env._scene._active_task.get_base().get_objects_in_tree(exclude_base=True, first_generation_only=False)
+        obj_poses = {}
+
+        for obj in objs:
+            name = obj.get_name()
+            pose = obj.get_pose()
+
+            pos, quat_wxyz = sample_normal_pose(self._pos_scale, self._rot_scale)
+            gt_quat_wxyz = quaternion(pose[6], pose[3], pose[4], pose[5])
+            perturbed_quat_wxyz = quat_wxyz * gt_quat_wxyz
+
+            pose[:3] += pos
+            pose[3:] = [perturbed_quat_wxyz.x, perturbed_quat_wxyz.y, perturbed_quat_wxyz.z, perturbed_quat_wxyz.w]
+
+            obj_poses[name] = pose
+
+        return obj_poses
+
+
+if __name__ == "__main__":
+    action_mode = ActionMode(ArmActionMode.DELTA_EE_POSE) # See rlbench/action_modes.py for other action modes
+    env = Environment(action_mode, '', ObservationConfig(), False)
+    task = env.get_task(BlockPyramid) # available tasks: StackCups, StackBlocks, BlockPyramid, SweepToDustpan
+    agent = RandomAgent()
+    obj_pose_sensor = NoisyObjectPoseSensor(env)
+   
+    descriptions, obs = task.reset()
+    print(descriptions)
+    while True:
+        # Getting noisy object poses
+        obj_poses = obj_pose_sensor.get_poses()
+
+        # Getting various fields from obs
+        current_joints = obs.joint_positions
+        gripper_pose = obs.gripper_pose
+        rgb = obs.wrist_rgb
+        depth = obs.wrist_depth
+        mask = obs.wrist_mask
+
+        # Perform action and step simulation
+        action = agent.act(obs)
+        obs, reward, terminate = task.step(action)
+
+        if terminate:
+            break
+
+    env.shutdown()
